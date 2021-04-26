cce2d142121e2d2eb91d664878bc8912c54e4ece
Zebedee <zebedeeslewis@gmail.com>
2021-04-26 10:00:24 -0600

    Create Skeleton Module For Todo Controller

---
+++ apps/presentation/web/api/src/app/controller/todo/todo-controller.spec.ts
@@ -0,0 +1,1 @@
+test.todo('Setup todo-controller tests')
--- apps/presentation/web/api/src/app/controller/todo/todo-controller.ts
+++ apps/presentation/web/api/src/app/controller/todo/todo-controller.ts
@@ -4,6 +4,11 @@ import * as HTTP from 'http-status-codes'
 /**
  * Handles GET requests to the API endpoint for todo items.
  *
+ * - it calls "get_single" if the request maps to the /api/v1/todo/:id
+ *   route.
+ * - it calls "get_collection" if the reqest maps to the /api/v1/todo
+ *   route.
+ *
  * @param req - the express Request object
  * @param res - the express Response object
  *
