ac6a67d9f3ec2b009e8435daf71c2ce4c9f94d9a
Zebedee <zebedeeslewis@gmail.com>
2021-05-08 14:13:16 -0600

    Update Todo Controller Test Format
      Statu Quo: The layout and ordering of unit test functions in the todo
      controller spec file is very haphazard and inconsistent. Locally
      defined functions are used within the tests themselves and
      constants used in individual tests are define at the top of the file
      far away from where they are actually used, this is done so that the
      constants can be reused by multiple tests.
    
      Problem: The layout of tests should emphasise easy visual parsing
      and reading over reusability. The current layout does not optimise
      for this, resulting in tests that are a bit hard to quickly visually
      parse, severly damaging the usefulness of the tests to anyone using
      them who does not understand the codebase.
    
      Solution: Update the test structure to reflect the best practices
      layed out in the node best practices document (see:
      https://github.com/goldbergyoni/nodebestpractices/blob/a340bce5c2642bdc8575c79de6c2d528d9948e63/sections/testingandquality/3-parts-in-name.md and https://github.com/goldbergyoni/nodebestpractices/blob/a340bce5c2642bdc8575c79de6c2d528d9948e63/sections/testingandquality/aaa.md)

--- apps/presentation/web/api/src/app/controller/todo/todo-controller.spec.ts
+++ apps/presentation/web/api/src/app/controller/todo/todo-controller.spec.ts
@@ -8,269 +8,426 @@ import * as TodoController from './todo-controller'
 import * as Repository from '@libs/infrastructure/repository'
 import * as Entity from '@libs/domain/entity'
 
-const VALID_ID_1 = 'dc965fe8-0868-44da-a0df-581010053655'
-const VALID_ID_2 = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
-
-const INVALID_ID_RANDOM = 'randome-id'
-const INVALID_ID_V1 = 'e4141c50-aa9e-11eb-9375-9ddbe18c35c9'
-const INVALID_ID_V3 = '641eb357-8761-3f64-b791-b2d4398e8532'
-const INVALID_ID_V5 = 'f81789d2-8ecc-5d84-b2e8-986e69b7f5d4'
-
-const repoDTO = {}
-const todoDTO: Entity.Todo.DTO = {
-  id: VALID_ID_1,
-  title: 'test title',
-}
-
 jest.mock('@libs/infrastructure/repository', () => {
+  const { Todo } = jest.requireActual('@libs/infrastructure/repository')
   return {
     Todo: {
-      find: jest.fn(async (id) => {
-        return Result.ok({ ...todoDTO, id })
-      }),
-      create: jest.fn(() => {
-        return todoDTO
-      }),
+      find: jest.fn(),
+      create: Todo.create,
     },
   }
 })
-
-const mockedSend = jest.fn(function () {
-  return this
-}) as jest.MockedFunction<typeof express.response.send>
-
-const mockedStatus = jest.fn(function (statusCode) {
-  return this
-}) as jest.MockedFunction<typeof express.response.status>
-
-const mockedNextFunction = jest.fn(function (deferToNext: string) {
-  return
-}) as jest.MockedFunction<NextFunction>
-
-const res = ({
-  status: mockedStatus,
-  send: mockedSend,
-} as unknown) as express.Response
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
-describe('get_single', () => {
-  it(
-    'responds with status code "OK" (i.e. calls res.status with ' +
-      '200) if a todo item whos "id" matches the id portion of ' +
-      'the route has been found.',
-    async () => {
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
-
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
+describe('Todo Controller', () => {
+  describe('get_single', () => {
+    it(
+      'responds with status code "OK" if a todo item exists with ' +
+        'the requested id',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) =>
+          Result.ok({} as Entity.Todo.DTO)
         )
 
-        expect(res.status).toHaveBeenLastCalledWith(HTTP.StatusCodes.OK)
-      }
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
-
-  it(
-    'responds with the DTO for the todo item whos "id" matches ' +
-      'the id portion of the route.',
-    async () => {
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
-        const todoDTO: Entity.Todo.DTO = {
-          id,
-          title: 'test title',
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
         }
 
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
-        )
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [[HTTP.StatusCodes.OK], [HTTP.StatusCodes.OK]]
 
-        expect(res.send).toHaveBeenLastCalledWith(todoDTO)
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.status.mock.calls[0],
+          res.status.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
+    it(
+      'responds with the DTO of the todo item with the ' +
+        'requested id',
+      async () => {
+        // Arrange
+        const todoDTO = {
+          title: 'test title',
+          details: 'random details',
+          creationTime: '2021-05-07T06:00:00.000Z',
+          completionTime: null,
+        }
 
-  it(
-    'responds with status code "BAD_REQUEST" (i.e. calls res.status ' +
-      'with 400) if the id portion of the route is not a valid id.',
-    async () => {
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
-
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) =>
+          Result.ok({ ...todoDTO, id })
         )
 
-        expect(res.status).toHaveBeenLastCalledWith(
-          HTTP.StatusCodes.BAD_REQUEST
-        )
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }) as jest.MockedFunction<typeof express.response.status>,
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
+
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
+
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [
+          [{ ...todoDTO, id: idA }],
+          [{ ...todoDTO, id: idB }],
+        ]
+
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.send.mock.calls[0],
+          res.send.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(INVALID_ID_RANDOM)
-      await do_test(INVALID_ID_V1)
-      await do_test(INVALID_ID_V3)
-      await do_test(INVALID_ID_V5)
-    }
-  )
-
-  it(
-    'responds with the reason phrase (i.e. calls res.send) for ' +
-      'status code "BAD_REQUEST" if the id portion of the route is ' +
-      'not a valid id.',
-    async () => {
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
-
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
+    it(
+      'responds with status code "Bad Request" if the id ' +
+        'provided by the caller is invalid.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) =>
+          Result.ok({} as Entity.Todo.DTO)
         )
 
-        expect(res.send).toHaveBeenLastCalledWith(
-          HTTP.ReasonPhrases.BAD_REQUEST
-        )
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
+
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
+
+        const randomId = 'randome-id'
+        const uuidV1 = 'e4141c50-aa9e-11eb-9375-9ddbe18c35c9'
+        const uuidV3 = '641eb357-8761-3f64-b791-b2d4398e8532'
+        const uuidV5 = 'f81789d2-8ecc-5d84-b2e8-986e69b7f5d4'
+
+        const expected = [
+          [HTTP.StatusCodes.BAD_REQUEST],
+          [HTTP.StatusCodes.BAD_REQUEST],
+          [HTTP.StatusCodes.BAD_REQUEST],
+          [HTTP.StatusCodes.BAD_REQUEST],
+        ]
+
+        // Act
+        await get_single({ params: { id: uuidV1 } as any })
+        await get_single({ params: { id: uuidV3 } as any })
+        await get_single({ params: { id: uuidV5 } as any })
+        await get_single({ params: { id: randomId } as any })
+        const recieved = [
+          res.status.mock.calls[0],
+          res.status.mock.calls[1],
+          res.status.mock.calls[2],
+          res.status.mock.calls[3],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(INVALID_ID_RANDOM)
-      await do_test(INVALID_ID_V1)
-      await do_test(INVALID_ID_V3)
-      await do_test(INVALID_ID_V5)
-    }
-  )
-
-  it(
-    'responds with status code "INTERNAL_SERVER_ERROR" (i.e. calls ' +
-      'res.status with 500) if there is an error in the retrieval ' +
-      'process.',
-    async () => {
-      ;(Repository.Todo.find as jest.MockedFunction<
-        typeof Repository.Todo.find
-      >).mockReturnValue(Promise.resolve(Result.err('random error')))
-
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
-
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
+    it(
+      'responds with the reason phrase for the "Bad Request" ' +
+        'if the id provided by the caller is invalid.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) =>
+          Result.ok({} as Entity.Todo.DTO)
         )
 
-        expect(res.status).toHaveBeenLastCalledWith(
-          HTTP.StatusCodes.INTERNAL_SERVER_ERROR
-        )
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
+
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
+
+        const randomId = 'randome-id'
+        const uuidV1 = 'e4141c50-aa9e-11eb-9375-9ddbe18c35c9'
+        const uuidV3 = '641eb357-8761-3f64-b791-b2d4398e8532'
+        const uuidV5 = 'f81789d2-8ecc-5d84-b2e8-986e69b7f5d4'
+
+        const expected = [
+          [HTTP.ReasonPhrases.BAD_REQUEST],
+          [HTTP.ReasonPhrases.BAD_REQUEST],
+          [HTTP.ReasonPhrases.BAD_REQUEST],
+          [HTTP.ReasonPhrases.BAD_REQUEST],
+        ]
+
+        // Act
+        await get_single({ params: { id: uuidV1 } as any })
+        await get_single({ params: { id: uuidV3 } as any })
+        await get_single({ params: { id: uuidV5 } as any })
+        await get_single({ params: { id: randomId } as any })
+        const recieved = [
+          res.send.mock.calls[0],
+          res.send.mock.calls[1],
+          res.send.mock.calls[2],
+          res.send.mock.calls[3],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
+    it(
+      'responds with status code "Internal Server Error" if there ' +
+        'was an error in the retrieval process.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) => Result.err('random error'))
 
-  it(
-    'responds with reason phrase (i.e. calls res.send) for status ' +
-      'code "INTERNAL_SERVER_ERROR" if there is an error in the ' +
-      'retrieval process.',
-    async () => {
-      ;(Repository.Todo.find as jest.MockedFunction<
-        typeof Repository.Todo.find
-      >).mockReturnValue(Promise.resolve(Result.err('random error')))
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
 
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
 
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
-        )
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [
+          [HTTP.StatusCodes.INTERNAL_SERVER_ERROR],
+          [HTTP.StatusCodes.INTERNAL_SERVER_ERROR],
+        ]
 
-        expect(res.send).toHaveBeenLastCalledWith(
-          HTTP.ReasonPhrases.INTERNAL_SERVER_ERROR
-        )
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.status.mock.calls[0],
+          res.status.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
+    it(
+      'responds with the reason phrase for the "Internal Server ' +
+        'Error" if the id provided by the caller is invalid.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) => Result.err('random error'))
 
-  it(
-    'responds with status code "NOT_FOUND" (i.e. calls res.status ' +
-      'with 404) if a todo item with the matching id cannot be found.',
-    async () => {
-      ;(Repository.Todo.find as jest.MockedFunction<
-        typeof Repository.Todo.find
-      >).mockReturnValue(Promise.resolve(Result.ok(null)))
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
 
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
 
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
-        )
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [
+          [HTTP.ReasonPhrases.INTERNAL_SERVER_ERROR],
+          [HTTP.ReasonPhrases.INTERNAL_SERVER_ERROR],
+        ]
 
-        expect(res.status).toHaveBeenLastCalledWith(
-          HTTP.StatusCodes.NOT_FOUND
-        )
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.send.mock.calls[0],
+          res.send.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
+    it(
+      'responds with status code "Not Found" if no todo item ' +
+        'with the requested id can be found.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) => Result.ok(null))
 
-  it(
-    'responds with reason phrase for status code "NOT_FOUND" (i.e. ' +
-      'calls res.status with 404) if a todo item with the matching ' +
-      'id cannot be found.',
-    async () => {
-      ;(Repository.Todo.find as jest.MockedFunction<
-        typeof Repository.Todo.find
-      >).mockReturnValue(Promise.resolve(Result.ok(null)))
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
 
-      async function do_test(id: string) {
-        const req = { params: { id } as any } as express.Request
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
 
-        await TodoController.get_single(
-          repoDTO,
-          req,
-          res,
-          mockedNextFunction
-        )
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [
+          [HTTP.StatusCodes.NOT_FOUND],
+          [HTTP.StatusCodes.NOT_FOUND],
+        ]
 
-        expect(res.send).toHaveBeenLastCalledWith(
-          HTTP.ReasonPhrases.NOT_FOUND
-        )
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.status.mock.calls[0],
+          res.status.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
       }
+    )
 
-      await do_test(VALID_ID_1)
-      await do_test(VALID_ID_2)
-    }
-  )
+    it(
+      'responds with the reason phrase for the "Not Found" ' +
+        'status code if no todo item with requeste id can be found.',
+      async () => {
+        // Arrange
+        ;(Repository.Todo.find as jest.MockedFunction<
+          typeof Repository.Todo.find
+        >).mockImplementation(async (id) => Result.ok(null))
+
+        const res = ({
+          status: jest.fn(function (s) {
+            return this
+          }),
+          send: jest.fn(function (s) {
+            return this
+          }),
+        } as unknown) as express.Response
+
+        const get_single = async (req) => {
+          await TodoController.get_single(
+            {},
+            req,
+            res,
+            jest.fn() as jest.MockedFunction<NextFunction>
+          )
+        }
+
+        const idA = 'dc965fe8-0868-44da-a0df-581010053655'
+        const idB = 'fd26f7f0-8436-4fdd-a25f-4c6a7ac9a009'
+        const expected = [
+          [HTTP.ReasonPhrases.NOT_FOUND],
+          [HTTP.ReasonPhrases.NOT_FOUND],
+        ]
+
+        // Act
+        await get_single({ params: { id: idA } as any })
+        await get_single({ params: { id: idB } as any })
+        const recieved = [
+          res.send.mock.calls[0],
+          res.send.mock.calls[1],
+        ]
+
+        // Assert
+        expect(recieved).toEqual(expected)
+      }
+    )
+  })
 })
